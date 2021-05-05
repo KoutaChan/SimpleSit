5bd2e2f35c35db87ef31811c58aa60752c20b897
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-05-06 07:24:23 +0900

    Upload

--- src/main/java/koutachan/simplesit/event.java
+++ src/main/java/koutachan/simplesit/event.java
@@ -1,5 +1,6 @@
 package koutachan.simplesit;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.type.Stairs;
@@ -15,7 +16,7 @@ public class event implements Listener{
 
     @EventHandler
     public void PlayerInteractEvent(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getPlayer().getVehicle() == null) {
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getPlayer().getVehicle() == null && e.getPlayer().getGameMode() != GameMode.SPECTATOR && !e.getPlayer().isSneaking()) {
             Material StairsCheck = e.getClickedBlock().getType();
             if (StairsCheck.equals(Material.OAK_STAIRS) || StairsCheck.equals(Material.SPRUCE_STAIRS) || StairsCheck.equals(Material.BIRCH_STAIRS) || StairsCheck.equals(Material.JUNGLE_STAIRS) || StairsCheck.equals(Material.ACACIA_STAIRS) || StairsCheck.equals(Material.DARK_OAK_STAIRS) || StairsCheck.equals(Material.CRIMSON_STAIRS) || StairsCheck.equals(Material.WARPED_STAIRS) || StairsCheck.equals(Material.STONE_STAIRS) || StairsCheck.equals(Material.COBBLESTONE_STAIRS) || StairsCheck.equals(Material.MOSSY_COBBLESTONE_STAIRS) || StairsCheck.equals(Material.STONE_BRICK_STAIRS) || StairsCheck.equals(Material.MOSSY_STONE_BRICK_STAIRS) || StairsCheck.equals(Material.ANDESITE_STAIRS) || StairsCheck.equals(Material.POLISHED_ANDESITE_STAIRS) || StairsCheck.equals(Material.DIORITE_STAIRS) || StairsCheck.equals(Material.POLISHED_DIORITE_STAIRS) || StairsCheck.equals(Material.GRANITE_STAIRS) || StairsCheck.equals(Material.POLISHED_GRANITE_STAIRS) || StairsCheck.equals(Material.SANDSTONE_STAIRS) || StairsCheck.equals(Material.SMOOTH_SANDSTONE_STAIRS) || StairsCheck.equals(Material.RED_SANDSTONE_STAIRS) || StairsCheck.equals(Material.SMOOTH_RED_SANDSTONE_STAIRS) || StairsCheck.equals(Material.BRICK_STAIRS) || StairsCheck.equals(Material.PRISMARINE_STAIRS) || StairsCheck.equals(Material.PRISMARINE_BRICK_STAIRS) || StairsCheck.equals(Material.DARK_PRISMARINE_STAIRS) || StairsCheck.equals(Material.NETHER_BRICK_STAIRS) || StairsCheck.equals(Material.RED_NETHER_BRICK_STAIRS) || StairsCheck.equals(Material.QUARTZ_STAIRS) || StairsCheck.equals(Material.SMOOTH_QUARTZ_STAIRS) || StairsCheck.equals(Material.PURPUR_STAIRS) || StairsCheck.equals(Material.END_STONE_BRICK_STAIRS) || StairsCheck.equals(Material.BLACKSTONE_STAIRS) || StairsCheck.equals(Material.POLISHED_BLACKSTONE_STAIRS) || StairsCheck.equals(Material.POLISHED_BLACKSTONE_BRICK_STAIRS)) {
                 if(((Stairs) e.getClickedBlock().getBlockData()).getHalf() == Bisected.Half.BOTTOM) {
